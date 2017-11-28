0ae0d0353f930bd605f4df0f88abe56b72d50149
azarovalex <alex@azarov.by>
2017-11-29 01:20:26 +0300

    GUI + file opening added

--- Halstead Complexity/ViewController.swift
+++ Halstead Complexity/ViewController.swift
@@ -8,20 +8,49 @@
 
 import Cocoa
 
+func browseFile() -> String {
+    let dialog = NSOpenPanel();
+    dialog.title                   = "Choose a file";
+    dialog.showsResizeIndicator    = true;
+    dialog.showsHiddenFiles        = false;
+    dialog.canCreateDirectories    = true;
+    dialog.allowsMultipleSelection = false;
+    
+    if (dialog.runModal() == NSApplication.ModalResponse.OK) {
+        let result = dialog.url
+        if (result != nil) {
+            return result!.path
+        }
+    } else { return "" }
+    return ""
+}
+
 class ViewController: NSViewController {
 
+    
+    @IBOutlet weak var n1: NSTextField!
+    @IBOutlet weak var n2: NSTextField!
+    @IBOutlet weak var bigN1: NSTextField!
+    @IBOutlet weak var bigN2: NSTextField!
+    @IBOutlet var f1: NSTextView!
+    @IBOutlet var f2: NSTextView!
+    @IBOutlet var code: NSTextView!
+    var filePath = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        code.font = NSFont(name: "Courier New", size: 13)
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
+    
+    @IBAction func Calculate(_ sender: Any) {
+    }
+    
+    @IBAction func OpenFile(_ sender: Any) {
+        filePath = browseFile()
+        guard FileManager.default.fileExists(atPath: filePath) else {
+            return
         }
+        try! code.string = String.init(contentsOf: URL.init(fileURLWithPath: filePath), encoding: .ascii)
     }
-
-
 }
 
