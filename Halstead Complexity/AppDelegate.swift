93c857574e5218d99263689e100a25b13096fa64
azarovalex <alex@azarov.by>
2017-11-28 20:15:27 +0300

    Initial Commit

---
+++ Halstead Complexity/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Halstead Complexity
+//
+//  Created by Alex Azarov on 28/11/2017.
+//  Copyright © 2017 Alex Azarov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Halstead Complexity/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Halstead Complexity
+//
+//  Created by Alex Azarov on 28/11/2017.
+//  Copyright © 2017 Alex Azarov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
